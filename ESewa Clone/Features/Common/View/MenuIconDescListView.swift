56c78df21473af5878deba391e0581563c5aae5b
swornim.shah <swornim.shah@f1soft.com>
2023-11-22 22:12:04 +0545

    Profile module => adding user profile screen with header + menu list view + points view

---
+++ ESewa Clone/Features/Common/View/MenuIconDescListView.swift
@@ -0,0 +1,53 @@
+//
+//  MenuIconDescListView.swift
+//  ESewa Clone
+//
+//  Created by swornim-shah on 22/11/2023.
+//
+
+import SwiftUI
+
+public struct MenuIconDescListView: View {
+    
+    public var spacing: CGFloat = 0
+    
+    public var menus: [Menu]
+    
+    public init(menus: [Menu], spacing: CGFloat = 0) {
+        self.menus = menus
+        self.spacing = spacing
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(Array(menus.enumerated()), id: \.offset) { index, item in
+                MenuIconDescItemView(menu: item)
+                    .padding(.bottom, index != menus.count - 1 ? spacing : .zero)
+            }
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+public struct MenuIconDescItemView: View {
+    public var menu: Menu
+    
+    public init(menu: Menu) {
+        self.menu = menu
+    }
+    
+    public var body: some View {
+        HStack(alignment: .top, spacing: 18) {
+            Image(systemName: "house.fill")
+                .toNavigationIcon()
+            VStack(alignment: .leading) {
+                Text(menu.name ?? "")
+                    .font(.body)
+                    .fontWeight(.medium)
+                
+                Text(menu.desc ?? "")
+                    .font(.caption)
+                    .fontWeight(.regular)
+            }
+        }
+    }
+}
--- ESewa Clone/Utils/Ext/CGFloatExt.swift
+++ ESewa Clone/Utils/Ext/CGFloatExt.swift
@@ -8,5 +8,7 @@
 import SwiftUI
 
 public extension CGFloat {
+    static var smallSize: CGFloat { 16 }
     static var largeSize: CGFloat { 40 }
+    static var xLargeSize: CGFloat { 80 }
 }
